ee44d2419ecb00d5723a4a4bee0480f011fa0d49
CritterCodes <jacobaengel55@gmail.com>
2025-08-07 18:49:48 -0500

    Fix remaining auth import paths - corrected all relative paths to root auth.js for build success

--- src/app/api/tasks/crud/route.js
+++ src/app/api/tasks/crud/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { auth } from '../../../../../auth';
+import { auth } from '../../../../auth';
 import { db } from '@/lib/database';
 import { ObjectId } from 'mongodb';
 import { generateTaskSku, generateShortCode } from '@/utils/skuGenerator';
